7a364877841a158f2985f1d0354da561b877b3d3
Tiane Erwee <tiane.erwee@gmail.com>
2020-01-22 09:06:42 +0200

    fix env variables

---
+++ teardown-db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection ({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+db.connect((err) => {
+  if (err) {
+      throw err;
+  }
+  console.log('Connected to database');
+});
+
+db.query('CREATE DATABASE `cypress-test`', (err, result, fields) => {
+  if (!!err) {
+    console.log(err);
+  } else {
+    db.end();
+  }
+});
